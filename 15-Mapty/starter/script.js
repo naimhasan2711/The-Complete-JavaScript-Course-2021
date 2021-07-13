c180a59aff8575fee539b3a22417501d2966ea8d
nakibhasan2711 <naimhasan2711@diu.edu.bd>
2021-07-13 14:21:50 +0600

    mapty app add google map and default marker on the map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,33 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//fetching the cuurent location of user using geolocation API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    //success function
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+      const coord = [latitude, longitude];
+
+      var map = L.map('map').setView(coord, 12);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coord)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    //error function
+    function () {
+      alert('Could not fetch your current location.');
+    }
+  );
+}
